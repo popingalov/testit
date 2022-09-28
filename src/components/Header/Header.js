08e21f11cbfe4fcf9db85bb0272b7b6456373857
Oleksandr Balitskiy <popingalov@gmail.com>
2022-09-28 15:58:21 +0300

    f

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import {ReactComponent  as LogoSvg} from '../../materials/icons/logo.svg'
+
+
+
+export default function Header() {
+    return <header>
+      <p><span><LogoSvg/> Finance</span> Ledger</p>
+              </header>
+}
